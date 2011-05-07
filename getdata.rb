7e5643194e7b28b3bc8f1a92a4c741d914d34b2f
latentflip <phil@latentflip.com>
2011-05-07 16:10:23 +0100

    Better

--- getdata.rb
+++ getdata.rb
@@ -10,7 +10,7 @@ File.open('eventdata.js', 'w') do |f|
   events[0..length].each do |e|
     unless e['event_desc'] == "Apprentice"
       if (e['event_latitude'] && e['event_latitude'].to_f != 0.0)
-        f.puts "{desc: '#{e['event_desc'].tr("'",'"')}', event_latitude: #{e['event_latitude']}, event_longitude: #{e['event_longitude']}, event_info: '#{e['event_info'].tr("'",'"')}', start_time: #{e['start_time_hhmm'].to_i}, end_time: #{e['end_time_hhmm']}, price: #{e['min_seat_price'].to_i} },"
+        f.puts "{desc: '#{e['event_desc'].tr("'",'"')}', event_latitude: #{e['event_latitude']}, event_longitude: #{e['event_longitude']}, event_info: '#{e['event_info'].tr("'",'"')}', start_time: #{e['start_time_hhmm'].to_i}, end_time: #{e['end_time_hhmm']}, price: #{e['min_seat_price'].to_i}, url: '#{e['event_url']}' },"
       end
     end
   end
